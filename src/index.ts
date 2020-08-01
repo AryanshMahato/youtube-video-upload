00b6445d978bfbf2898318ede9249b08562af0b2
Aryansh Mahato <aryanshmahato@gmail.com>
2020-08-01 14:25:38 +0530

    Configuration Completed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { startServer } from "./startServer";
 import { uploadToYoutube } from "./controller/UploadToYoutube";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = startServer();
 
+//? Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+//? Routes
 app.post("/upload", uploadToYoutube);
